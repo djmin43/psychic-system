d2350408530ae569d031a43ae84a64e328a4feb8
jay <dj.min43@gmail.com>
2023-03-18 14:51:08 +0900

    reservation repository

--- parkinglot/src/main/java/mindongjoon/parkinglot/repository/ReservationRepository.java
+++ parkinglot/src/main/java/mindongjoon/parkinglot/repository/ReservationRepository.java
@@ -1,4 +1,24 @@
 package mindongjoon.parkinglot.repository;
 
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import mindongjoon.parkinglot.domain.Reservation;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ReservationRepository {
+
+    private final EntityManager em;
+
+    @Transactional
+    public void save(Reservation reservation) {
+       em.persist(reservation);
+    }
+
+    public Reservation findOne(Long id) {
+        return em.find(Reservation.class, id);
+    }
 }
