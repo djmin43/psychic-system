1b564879ee05bd4f17983c35fdadb53dc6155d96
jay <dj.min43@gmail.com>
2023-03-18 14:59:00 +0900

    create reservation

--- parkinglot/src/main/java/mindongjoon/parkinglot/domain/Reservation.java
+++ parkinglot/src/main/java/mindongjoon/parkinglot/domain/Reservation.java
@@ -3,11 +3,13 @@ package mindongjoon.parkinglot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 public class Reservation {
 
     @Id
@@ -16,13 +18,25 @@ public class Reservation {
     private Long id;
 
     @NotBlank
-    private LocalDateTime startTime;
+    private LocalDateTime startAt;
 
     @NotBlank
-    private LocalDateTime endTime;
+    private LocalDateTime endAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    // domain methods
+    public static Reservation createReservation(LocalDateTime startAt, LocalDateTime endAt, Member member) {
+        Reservation reservation = new Reservation();
+        reservation.setStartAt(startAt);
+        reservation.setEndAt(endAt);
+        reservation.setMember(member);
+        return reservation;
+
+
+    }
+
 
 }
