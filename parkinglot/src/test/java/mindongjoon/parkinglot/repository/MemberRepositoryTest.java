01f2ab402617fdea18d595fbd2bdf723e542fc28
jay <dj.min43@gmail.com>
2023-03-18 14:43:40 +0900

    write test for find one

--- parkinglot/src/test/java/mindongjoon/parkinglot/repository/MemberRepositoryTest.java
+++ parkinglot/src/test/java/mindongjoon/parkinglot/repository/MemberRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -22,13 +24,23 @@ public class MemberRepositoryTest {
 
     @Test
     public void save() {
+        Member newMember = getNewMember();
+        assertThat(newMember.getName()).isEqualTo("min");
+
+    }
+
+    private Member getNewMember() {
         Member newMember = Member.createMember("min", "1234", "dj.min43@gmail.com");
         memberRepository.save(newMember);
-        Assertions.assertThat(newMember.getName()).isEqualTo("min");
-
+        return newMember;
     }
 
     @Test
     public void findOne() {
+        Member newMember = getNewMember();
+        Member findMember = memberRepository.findOne(newMember.getId());
+        System.out.println("newMemberId = " + findMember.getId());
+        System.out.println("newMemberId = " + newMember.getId());
+        assertThat(findMember.getId()).isEqualTo(newMember.getId());
     }
 }
